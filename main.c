deff93f3c60323a17ff673dfc7c688cd58658f10
RyuJinHa <bb_jinha98@naver.com>
2018-11-01 16:23:47 +0900

    실습2

--- main.c
+++ main.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "calcMatrix.h"
-
+#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-int main(int argc, char *argv[]) {
+int main(void){
+	
+	int nums[SIZE];
+	int i;
+	
+	printf("input %i numbers: ", SIZE);
+	for(i=0;i<SIZE;i++)
+	{
+		scanf("%d ",&nums[i]);
+	}
 	
 	
-	int A[ROWS][COLS]={
-		{2,3,0},
-		{8,9,1},
-		{7,0,5}
-		};
-
-	int B[ROWS][COLS]={
-		{1,0,0},
-		{0,1,0},
-		{0,0,1}
-		};
+	//sorting function
+	//bubblsesort();
+	
 		
-	int C[ROWS][COLS];
-	
-	addMatrix(A,B,C);
-	printMatrix(C);
+	printf("sorted numbers are: ");
+	for (i=0;i<SIZE;i++)
+	{
+		printf("%d ", nums[i]);
+	}
 	
 	return 0;
+	
 }
